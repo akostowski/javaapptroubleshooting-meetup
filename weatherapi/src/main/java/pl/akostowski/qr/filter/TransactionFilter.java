9d33cd753c4786dfdf11ca46a761994b8384025b
AleksanderKostowski <aleksander.kostowski@gmail.com>
2023-05-08 09:36:32 +0200

    Upgrade to Java 17 and Spring Boot 3

--- weatherapi/src/main/java/pl/akostowski/qr/filter/TransactionFilter.java
+++ weatherapi/src/main/java/pl/akostowski/qr/filter/TransactionFilter.java
@@ -1,15 +1,11 @@
 package pl.akostowski.qr.filter;
 
+import jakarta.servlet.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import java.io.IOException;
 
 @Component
@@ -21,7 +17,7 @@ public class TransactionFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
-            Thread.sleep(10000l);
+            Thread.sleep(10000L);
         } catch (InterruptedException e) {
             logger.error("Thread interrupted", e);
         }
