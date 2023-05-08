9ae4b9920ddbb6bd330807c02e49a017971d28e5
AleksanderKostowski <aleksander.kostowski@gmail.com>
2023-05-08 11:11:20 +0200

    Add logger to weather api

--- weatherapi/src/main/java/pl/akostowski/qr/rest/WeatherController.java
+++ weatherapi/src/main/java/pl/akostowski/qr/rest/WeatherController.java
@@ -1,5 +1,7 @@
 package pl.akostowski.qr.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,11 @@ public class WeatherController {
 
     List<String> weather = Arrays.asList("sunny", "rainy", "windy", "cloudy", "snowy", "stormy");
 
+    Logger logger = LoggerFactory.getLogger(WeatherController.class);
+
     @GetMapping("/")
     public String get() {
+        logger.info("Get weather");
         Collections.shuffle(weather);
         return weather.get(0);
     }
